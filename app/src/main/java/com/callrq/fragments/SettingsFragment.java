6803f990eac53e14cbff68f5d4bd725792579b23
Adrian Popa <adipopa9910@gmail.com>
2018-04-06 17:53:15 +0300

    Fixed fragment onAttach() method bug on older APIs

--- app/src/main/java/com/callrq/fragments/SettingsFragment.java
+++ app/src/main/java/com/callrq/fragments/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.callrq.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -38,6 +40,19 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        onAttachListener(context);
+    }
+
+    // Method is deprecated since API 23 but must be defined because older APIs still call this method instead of the new one
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (Build.VERSION.SDK_INT < 23) {
+            onAttachListener(activity);
+        }
+    }
+
+    private void onAttachListener(Context context) {
         if (context instanceof OnFragmentCompleteListener) {
             mListener = (OnFragmentCompleteListener) context;
         } else {
